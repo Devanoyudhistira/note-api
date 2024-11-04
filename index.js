853a2f4e39c06736f712850ef0560d15d32675fd
Devanoyudhistira <devanotira@gmail.com>
2024-11-04 16:24:31 +0700

    fix:fix json bug

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const app = express()
 const host = 2220
 import getnote from "./routes/note/getnote.js"
 const uri = process.env.MONGOURL;
+import cors from "cors";
+
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
 
 app.use("/getnote",getnote)
 
 app.get("/",(req,res) => {
-    res.json({nama:"devano yudhistira",note:uri})
+    res.json({nama:"devano yudhistira"})
 })
 
 app.listen(host,() => console.log(`http://localhost:${host}`))
+
+export default app
