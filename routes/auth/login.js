65034964d70827ef240c84403861b4c77fe04621
Devanoyudhistira <devanotira@gmail.com>
2024-11-08 16:40:34 +0700

    fix:fix login

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -24,36 +24,22 @@ const connection = async () => {
 };
 
 loginuser.post("/login", async (req, res) => {
-  const { username } = req.body;
+  const { username ,nickname} = req.body;
   if (username) {
     const loginuser = await client.db(database).collection(userdb);
     const searchresult = await loginuser.find({ username: username }).toArray();
     if (searchresult.length === 0) {
-      res.status(404).json({ massage: "failed", status: 404 });
+      userconnect.insertOne({username:username,nickname:nickname});
+      res.status(204).json({ massage: "add new user", status: 202 ,data:req.body});
     } else {
       res
         .status(202)
-        .json({ massage: "login success", data: loginuser, status: 200 });
+        .json({ massage: "login success", data: loginuser, status: 200,data:req.body });
     }
   } else {
     res.status(408).json({ massage: "bad request", status: 408 });
   }
 });
 
-loginuser.post("/signin", async (req, res) => {
-  const { username } = req.body;
-  if (username) {
-    const userconnect = await client.db(database).collection(userdb);
-    const checkuser = await userconnect.find({ username: username }).toArray();
-    if (checkuser.length === 0) {
-      userconnect.insertOne({username:username});
-      res.status(200).json({ massage: "sign in success", stnatus: 200 });
-    } else {
-      res.status(400).json({ massage:"you already have an account", status: 400 });
-    }
-  } else {
-    res.status(408).json({ massage: "bad request", status: 408 });
-  }
-});
 
 export default loginuser;
